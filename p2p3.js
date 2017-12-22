d5ccc7e3d91e2738a60116b31624f6c993d0c11a
Claudiu Filip <claudiu.Filip@ness.com>
2017-12-22 16:03:16 +0200

    Initial non functioning code refactor

--- p2p3.js
+++ p2p3.js
@@ -1,22 +1,105 @@
-var sc = new WebSocket(`wss://local:8080`);
+var socket = new WebSocket('wss://local:8080');
 
-Peer.configure({
-    webSocket: sc
-});
+var peer = {};
 
-function addPeers(count) {
-    var peerPromises = [];
-    while (count--) {
-        peerPromises.push(new Peer());
-    }
-    return Promise.all(peerPromises);
-}
+peer.offer = () => {
+    let offer = new Connection();
 
-addPeers(3).then(function(peers) {
-    Peer.broadcast('foo');
-    Peer.on('bar', function(payload) {
-        console.log(payload);
+    return offer.connection
+        .createOffer()
+        .then(desc => {
+            let promiseDesc = offer.connection
+                .setLocalDescription(desc)
+                .then(() => desc);
+            let promiseIce = offer.getIceCandidate();
+            return Promise.all([promiseDesc, promiseIce]);
+        })
+        .then(([desc, ice]) => {
+            console.log("offer sent", desc, ice);
+            sendMessage({
+                type: "sendOffer",
+                data: {
+                    uuid,
+                    desc,
+                    ice,
+                    id: offer.id
+                }
+            });
+        })
+        .then(() => {
+            return new Promise((resolve, reject) => {
+                socket.onmessage = message => {
+                    let action = JSON.parse(message.data);
+
+                    if (action.type === "answer") {
+                        resolve(action.data);
+                    }
+                };
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+peer.ask = () => {
+    socket.sendMessage({
+        type: "requestOffer"
     });
 
-    peers[0].send('foobar');
+    return new Promise((resolve, reject) => {
+        socket.onmessage = message => {
+            let action = JSON.parse(message.data);
+
+            if (action.type === "offer") {
+                resolve(action.data);
+            }
+        };
+    });
+};
+
+var peer1 = peer.offer().then(answer => connection);
+
+var peer2 = peer.ask().then(() => {
+    let answer = new AnswerConnection();
+
+    return answer.connection
+        .setRemoteDescription(new RTCSessionDescription(offer.desc))
+        .then(() => peer.localConnection.createAnswer())
+        .then(desc => {
+            let promiseDesc = peer.localConnection
+                // set local description to local desc
+                .setLocalDescription(desc)
+                .then(() => desc);
+            let promiseIce = peer.getIceCandidate();
+            return Promise.all([promiseDesc, promiseIce]);
+        })
+        .then(([desc, ice]) => {
+            return peer.localConnection
+                .addIceCandidate(new RTCIceCandidate(ice))
+                .then(() => [desc, ice]);
+        })
+        .then(([desc, ice]) => {
+            console.log("answer sent", desc, ice);
+            sendMessage({
+                type: "sendAnswer",
+                data: {
+                    uuid,
+                    target: offer.uuid,
+                    desc,
+                    ice,
+                    offerId: offer.id,
+                    answerId: peer.id
+                }
+            });
+            return answer;
+        })
+        .catch(function(err) {
+            console.log("create answer desc error", err);
+        });
+    
 });
+
+
+peer1.send('atrrda');
+peer1.request('adasda').then();
--- peer.js
+++ peer.js
@@ -1,55 +1,53 @@
 class Connection {
-    constructor(type) {
-        var localConnection, sendChannel, receiveChannel;
-    var target = {
-        uuid: null
-    };
-
-    localConnection = new RTCPeerConnection(config);
-    sendChannel = localConnection.createDataChannel(`channel`);
-
-    localConnection.ondatachannel = function(event) {
-        receiveChannel = event.channel;
-        receiveChannel.onmessage = function(message) {
-            var payload = JSON.parse(message.data);
-
-            if (receivedMessages.indexOf(payload.data.id) === -1) {
-                receivedMessages.push(payload.data.id);
-                commitMessage(payload.data.value);
-                broadcast(message.data);
-            }
-        };
-        receiveChannel.onopen = function() {
-            console.log('receive datachannel opened', receiveChannel.readyState);
-        };
-
-        receiveChannel.onclose = function() {
-            console.log('receive datachannel closed', receiveChannel.readyState);
-        };
-    };
-
-    sendChannel.onopen = function() {
-        console.log('send datachannel opened', sendChannel.readyState);
-    };
-    sendChannel.onclose = function() {
-        console.log('send datachannel closed', sendChannel.readyState);
+    constructor(target) {
+        this.target = target;
+        this.id = Math.floor(Math.random() * 100000);
         
-    };
+        this.connection = new RTCPeerConnection(config);
 
-    sendChannel.onerror = function(err) {
-        console.log(err);
-    };
+        this.sendChannel = this.connection.createDataChannel(`channel`);
 
-    window.onbeforeunload = function() {
-        sendChannel.close();
-        if (receiveChannel) {
-            receiveChannel.close();
+        this.connection.ondatachannel = (event) => {
+            this.receiveChannel = event.channel;
+            this.receiveChannel.onmessage = (message) => {
+                let payload = JSON.parse(message.data);
+
+                this.handlers.forEach((handler) => {
+                    handler(payload);
+                });
+            };
+            this.receiveChannel.onopen = () => {
+                console.log('receive datachannel opened', this.receiveChannel.readyState);
+            };
+
+            this.receiveChannel.onclose = () => {
+                console.log('receive datachannel closed', this.receiveChannel.readyState);
+            };
+        };
+
+        this.sendChannel.onopen = () => {
+            console.log('send datachannel opened', this.sendChannel.readyState);
+        };
+        this.sendChannel.onclose = () => {
+            console.log('send datachannel closed', this.sendChannel.readyState);
+        };
+
+        this.sendChannel.onerror = (err) => {
+            console.log(err);
+        };
+
+        window.onbeforeunload = () => {
+            this.sendChannel.close();
+            if (this.receiveChannel) {
+                this.receiveChannel.close();
+            }
         }
+
     }
 
-    function getIceCandidate() {
+    getIceCandidate() {
         return new Promise((resolve, reject) => {
-            localConnection.onicecandidate = function(event) {
+            this.connection.onicecandidate = (event) => {
                 if (event.candidate !== null) {
                     resolve(event.candidate);
                 }
@@ -57,162 +55,20 @@ class Connection {
         });
     }
 
-
-    return {
-        sendChannel,
-        receiveChannel,
-        localConnection,
-        getIceCandidate,
-        target,
-        id: Math.floor(Math.random() * 100000)
-    };
-    }
-}
-class Offer {
-    constructor(socket) {
-        this.socket = socket;
-    }
-}
-
-class Answer {
-    constructor(socket) {
-        socket.filter()
-    }
-}
-class Peer {
-    
-
-    static broadcast() {
-        
+    on(type, handler) {
+        this.handlers[type] = (this.handlers[type] || []).push(handler);
+        return this;
     }
 
-    static configure (options) {
-        Peer.config = options;
+    off(type, handler) {
+        this.handlers[type] = (this.handlers[type] || []).filter(item => item !== handler);
+        return this;
+    }
 
-        Peer.configure.webSocket.onmessage = function(message) {
-            var action = JSON.parse(message.data);    
-            console.log(action);
-            switch(action.type) {
-                case 'newOffer': 
-                    requestOffer();
-                    break;
-                case 'offer': 
-                    answerOffer(action.data);
-                    break;
-                case 'answer':
-                    completeConnection(action.data);
-                    break;
-            }
+    send(message) {
+        if (typeof message !== 'string') {
+            message = JSON.stringify(message);
         }
+        this.sendChannel.send(message);
     }
-
-    constructor() {
-        var offer = new Offer(Peer.socket);
-        sendPeerInfo();
-        sendOffer();
-        requestOffer();
-    }
-
-    send() {
-
-    }
-    answerOffer(offer) {
-        if (!offer) {
-            return false;
-        }
-    
-        if (!offer.desc || offer.uuid === uuid || connectedUuids.indexOf(offer.uuid) !== -1) {
-            return;
-        }
-    
-        if (this.connectedUuids.indexOf(offer.uuid) === -1) {
-            this.connectedUuids.push(offer.uuid);
-        }
-    
-        var peer = createPeerConnection('answer');
-    
-        answerPeers[peer.id] = peer;
-        peer.target.uuid = offer.uuid;
-    
-        console.log('getOffer', offer.desc);
-        peer.localConnection
-            // set remote description to remote desc
-            .setRemoteDescription(new RTCSessionDescription(offer.desc))
-            .then(() => peer.localConnection.createAnswer())
-            .then((desc) => {
-                let promiseDesc = peer.localConnection
-                // set local description to local desc
-                    .setLocalDescription(desc)
-                    .then(() => desc);
-                let promiseIce = peer.getIceCandidate();
-                return Promise.all([promiseDesc, promiseIce]);
-            })
-            .then(([desc, ice]) => {
-                return peer.localConnection.addIceCandidate(new RTCIceCandidate(ice))
-                    .then(() => [desc, ice]);
-            })
-            .then(([desc, ice]) => {
-                console.log('answer sent', desc, ice);
-                sendMessage({
-                    type: 'sendAnswer',
-                    data: {
-                        uuid,
-                        target: offer.uuid,
-                        desc,
-                        ice,
-                        offerId: offer.id,
-                        answerId: peer.id
-                    }
-                });
-            })
-            .catch(function(err) {
-                console.log('create answer desc error', err);
-            });
-    }
-    sendOffer() {
-        let peer = createPeerConnection('offer');
-        Peer.offerPeers[peer.id] = peer;
-    
-        return peer.localConnection
-            .createOffer()
-            .then((desc) => {
-                let promiseDesc = peer.localConnection
-                    .setLocalDescription(desc)
-                    .then(() => desc);
-                let promiseIce = peer.getIceCandidate();
-                return Promise.all([promiseDesc, promiseIce]);
-            })
-            .then(([desc, ice]) => {
-                console.log('offer sent', desc, ice)
-                Peer.sendToServer({
-                    type: 'sendOffer',
-                    data: {
-                        uuid,
-                        desc,
-                        ice,
-                        id: peer.id
-                    }
-                });
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    }
-
-    sendPeerInfo() {
-        Peer.sendToServer({
-            type: 'sendPeerInfo',
-            data: {
-                uuid: this.uuid
-            }
-        });
-    }
-
-    requestOffer() {
-        Peer.sendToServer({
-            type: 'requestOffer'
-        });
-    }
-
-    
 }
